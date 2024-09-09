5393142a3799e921dd8ebb153064138a61ba8232
Tim Martin <tim@timmart.in>
2024-09-09 12:36:48 -0500

    Optimize font loading with next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,16 @@ import "./app.css";
 import { appName } from "@/lib/constants";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import localFont from 'next/font/local'
+ 
+// Font files can be colocated inside of `app`
+const runescapeFont = localFont({
+  src: './fonts/RuneScape-UF.woff2',
+  display: 'swap',
+  weight: 'normal',
+  style: 'normal',
+  variable: '--font-runescape',
+})
 
 export const metadata: Metadata = {
   title: appName,
@@ -15,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${runescapeFont.variable}`}>
       <body>
         <Header />
         <main className="container mx-auto p-8">{children}</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,7 +44,8 @@ const config: Config = {
         border: "hsl(from var(--border) h s l / <alpha-value>)",
       },
       fontFamily: {
-        runescape: ['"RuneScape UF"', ...defaultTheme.fontFamily.mono],
+        // runescape: ['"RuneScape UF"', ...defaultTheme.fontFamily.mono],
+        runescape: ['var(--font-runescape)', ...defaultTheme.fontFamily.mono],
       },
     },
   },
