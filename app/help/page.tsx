a44bf9023dcc5cae2dd0fe2789c81a0374dc3d56
Tim Martin <tim@timmart.in>
2024-12-19 10:56:45 -0600

    Remove faker dependency

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { faker } from "@faker-js/faker";
 
 export default function Help() {
   return (
@@ -7,23 +6,34 @@ export default function Help() {
       <h1 className="text-6xl font-runescape text-primary">Help</h1>
 
       <div className="space-y-4">
-        <p>{faker.lorem.paragraph(5)}</p>
+        <p>
+          Uberrime comis cicuta attonbitus ager triduana repudiandae. Aperte
+          defluo cupiditate. Debeo adaugeo cado caterva triumphus synagoga. Unde
+          vitium vox subnecto. Adiuvo xiphias vulnero dolore agnosco accusator
+          cuius.
+        </p>
+
+        <img src="/barrows.png" alt="Random image" className="mx-auto w-1/2" />
+
+        <p>
+          Tres tempora antea creta tendo. Vix aequus molestias deputo. Claudeo
+          terga convoco aranea caterva tertius cohaero provident aliqua. Depulso
+          aequitas utrimque auctus. Deduco carbo ancilla curo vesco curo
+          crepusculum templum.
+        </p>
 
         <img
-          src={faker.image.url({ height: 200, width: 300 })}
+          src="/colusseum.jpg"
           alt="Random image"
-          className="mx-auto"
+          className="mx-auto w-1/2"
         />
 
-        <p>{faker.lorem.paragraph(5)}</p>
-
-        <img
-          src={faker.image.url({ height: 200, width: 300 })}
-          alt="Random image"
-          className="mx-auto"
-        />
-        
-        <p>{faker.lorem.paragraph(5)}</p>
+        <p>
+          Statim inflammatio iusto cur. Despecto urbs vilis territo conspergo
+          compello vado stips uter. Audax decipio somnus. Decumbo circumvenio
+          admoveo subvenio tricesimus studio advenio tondeo compono. Abundans
+          sed volutabrum tersus titulus cunctatio.
+        </p>
       </div>
     </div>
   );
